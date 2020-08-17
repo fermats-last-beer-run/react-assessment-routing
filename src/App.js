e957be7db4ee9b6fde2a59e33cf85be716d10ec4
Dylan Espey <dylanespey@gmail.com>
2020-08-17 13:47:19 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch} from 'react-router-dom';
 import './App.css';
+import Welcome from './components/Welcome'
+import Contact from './components/Contact';
+import Clock from './components/Clock'
+import NoMatch from './components/NoMatch'
+import Navigation from './components/Navigation'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* render the Navigation component */}
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render = { (props) => <Welcome {...props} name='Dylan' /> } 
+        />
+        <Route
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+      <Route path="/clock" component={Clock} />
+      <Route path="/contact" component={Contact} />
+      <Route>
+          <NoMatch />
+      </Route>
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+    /*** Copy this code block to Welcome.js ***/
+    import React from 'react';
+    
+    function Welcome(props) {
+        return (
+            <div>
+                <h1>Hello, {props.name}!</h1>
+            </div>
+        );
+    }
+    export default Welcome;
